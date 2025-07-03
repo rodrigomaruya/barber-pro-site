f445d30ec636ee3f2bc071c029054741efef5a76
Rodrigo <rod.maruya@gmail.com>
2025-07-03 14:05:24 +0900

    corrigindo  bug no auth

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -1,13 +1,8 @@
 import { setCookie } from "nookies";
 
 export function setAuthTokenCookie(token: string, ctx = null) {
-  const isProd = process.env.NODE_ENV === "production";
-
   setCookie(ctx, "@barber.token", token, {
     maxAge: 60 * 60 * 24 * 30, // 30 dias
     path: "/",
-    secure: isProd, // necessário para SameSite: 'none'
-    sameSite: isProd ? "none" : "lax", // 'none' precisa de HTTPS
-    httpOnly: true, // protege contra JS no client
   });
 }
